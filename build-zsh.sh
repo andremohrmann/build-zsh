6343437460cda2271a6f4b034d6596b976a2028a
André Mohrmann <andremohrmann@users.noreply.github.com>
2017-12-04 19:03:43 +0100

    Damn sapce...

--- build-zsh.sh
+++ build-zsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Build zsh from http://zsh.sourceforge.net/Arc/git.html and sources INSTALL file on debian 
+# Build zsh from http://zsh.sourceforge.net/Arc/git.html and sources INSTALL file on debian
 # Purge previous versions (from packages) before installing from source!
 
 # Make exits on error
